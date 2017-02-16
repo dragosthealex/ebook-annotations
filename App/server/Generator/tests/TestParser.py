380c6dd68d517b14469c8162f5f7e32108a5d656
Alex Radu <dragosthealx@gmail.com>
2017-02-16 07:50:42 +0000

    removed prototypes

--- App/server/Generator/tests/TestParser.py
+++ App/server/Generator/tests/TestParser.py
@@ -26,4 +26,18 @@ class TestParser(unittest.TestCase):
       if (t != '') and (t is not None) and (a != '') and (a is not None) \
          and (ch is not None) and (len(ch) > 1):
          success += 1
+    self.assertTrue(success > 90)
+    print(success)
+
+  def test_parse_random_100_old(self):
+    success = 0
+    for i in range(100):
+      self.setUp()
+      t = self.p.get_title()
+      a = self.p.get_author()
+      ch = self.p.get_chapters_2()
+      if (t != '') and (t is not None) and (a != '') and (a is not None) \
+         and (ch is not None) and (len(ch) > 1):
+         success += 1
+    self.assertTrue(success > 90)
     print(success)
--- App/server/prototypes/Generator/tests/context.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../smart_ebook_generator'))
-sys.path.insert(0, os.path.abspath('../'))
-from html_book_parser import *
-from text_analysis import *
-from url_searcher import *
-from Utils import *
