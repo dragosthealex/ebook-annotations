78b1cba920cfed258001eb4140b787865b52e4fd
Alex Radu <dragosthealx@gmail.com>
2017-02-16 08:00:04 +0000

    Added some intro text on homepage

--- App/front_end/www/index.php
+++ App/front_end/www/index.php
@@ -4,7 +4,17 @@ $header['title'] = 'Annotated eBook Reader';
 include '../templates/header.php';
 ?>
 <div class="row" id="book-results">
-
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h2>Welcome to EbookAnnotations</h2>
+    </div>
+    <div class="panel-body">
+      <p>
+        Use the search box to search for any book by title. Please be advised that
+        annotations currently work only for the first chapter, on the English books.
+      </p>
+    </div>
+  </div>
 </div>
 <?php
 include '../templates/footer.php';
